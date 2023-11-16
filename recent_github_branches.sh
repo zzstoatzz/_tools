afdbd77457638d9a0e0f77c01d6643ba6f259e00
nate nowack <thrast36@gmail.com>
2023-11-16 00:29:49 -0600

    1984

--- recent_github_branches.sh
+++ recent_github_branches.sh
@@ -1,6 +1,6 @@
 function gbre() {
     local count=${1:-10}
-    local email=${2:-"thrast36@gmail.com"}
+    local email=${2:-"ohNoMyEmail@gmail.com"}
     
     for branch in $(git for-each-ref --sort=-committerdate refs/heads --format="%(refname:short)"); do
         if git log -1 --author="$email" $branch > /dev/null; then

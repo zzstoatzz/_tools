b9e486c54b4e6320d02242696801a463c5820abb
Nathan Nowack <thrast36@gmail.com>
2023-11-16 00:26:40 -0600

    init

---
+++ recent_github_branches.sh
@@ -0,0 +1,11 @@
+function gbre() {
+    local count=${1:-10}
+    local email=${2:-"thrast36@gmail.com"}
+    
+    for branch in $(git for-each-ref --sort=-committerdate refs/heads --format="%(refname:short)"); do
+        if git log -1 --author="$email" $branch > /dev/null; then
+            echo $branch
+        fi
+    done | head -n $count
+}
+
